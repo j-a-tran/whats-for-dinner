96d2473fa854428edc803ec2891fcd7835841ca2
j-a-tran <jaclyn.a.tran@gmail.com>
2020-05-12 18:28:39 -0400

    changing index page

--- dinner_project/dinner_project/urls.py
+++ dinner_project/dinner_project/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('recipes/', include('recipes.urls')),
+    path('', include('recipes.urls')),
     path('admin/', admin.site.urls),
     #path('accounts/', include('django.contrib.auth.urls')),
 ]
