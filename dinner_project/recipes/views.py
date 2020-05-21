58e1221f2548b69cf828c2cb3cb3b76f8e23a632
j-a-tran <jaclyn.a.tran@gmail.com>
2020-05-21 18:00:57 -0400

    adding recipe API endpoints to views

--- dinner_project/recipes/views.py
+++ dinner_project/recipes/views.py
@@ -4,11 +4,15 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 
 import random
 
 from .forms import SearchForm
 from recipes.models import Recipe, Ingredient
+from .serializers import *
 
 # Create your views here.
 
@@ -64,3 +68,35 @@ class RecipeDeleteView(DeleteView):
     model = Recipe
     success_url = reverse_lazy('index')
     template_name_suffix = '_delete'
+
+@api_view(['GET','POST'])
+def recipes_list(request):
+    if request.method == 'GET':
+        data = Recipe.objects.all()
+
+        serializer  = RecipeSerializer(data, context={'request': request}, many=True)
+
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = RecipeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT', 'DELETE'])
+def recipes_detail(request, pk):
+    try:
+        recipe = Recipe.objects.gets(pk=pk)
+    except Recipe.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'PUT':
+        serializer = RecipeSerializer(recipe, data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        recipe.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
