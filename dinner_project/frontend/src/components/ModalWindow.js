2d0e18811f04a720885ad2ef47025ac871f02759
j-a-tran <jaclyn.a.tran@gmail.com>
2020-05-28 22:48:01 -0400

    cleanup

--- dinner_project/frontend/src/components/ModalWindow.js
+++ dinner_project/frontend/src/components/ModalWindow.js
@@ -3,9 +3,7 @@ import Dialog from '@material-ui/core/Dialog';
 import Button from '@material-ui/core/Button';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
-import RecipeForm from './RecipeForm';
 
 export default function ModalWindow(props) {
 
