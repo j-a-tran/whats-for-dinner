af429ee3c276d30b92efe87585ae51628679de2f
j-a-tran <jaclyn.a.tran@gmail.com>
2020-06-04 17:49:52 -0400

    add basic app bar

--- dinner_project/frontend/src/App.js
+++ dinner_project/frontend/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Page from './components/Page';
 import Login from './components/Login';
 import Register from './components/Register';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
 
 import { Switch, Route, NavLink, Link } from "react-router-dom";
 
@@ -9,23 +12,27 @@ function App() {
   return (
     <div className="App">
       <nav>
-        <ul>
-          <li>
-            <NavLink to='/register'>Register</NavLink>
-          </li>
-          <li>
-            <NavLink to='/login'>Log In</NavLink>
-          </li>
-          <li>
-            <NavLink to='/'>Home</NavLink>
-          </li>
-        </ul>
+        <AppBar position='static'>
+          <Toolbar>
+            <Button component={NavLink} to='/register' color='inherit'>
+              Register
+            </Button>
+            <Button component={NavLink} to='/login' color='inherit'>
+              Log In
+            </Button>
+            <Button component={NavLink} to='/' color='inherit'>
+              Home
+            </Button>   
+          </Toolbar>
+        </AppBar>
       </nav>
-      <Switch>
-        <Route path='/register' component={Register} />
-        <Route path='/login' component={Login} />
-        <Route path='/' component={Page} />
-      </Switch>
+      <div>
+        <Switch>
+          <Route path='/register' component={Register} />
+          <Route path='/login' component={Login} />
+          <Route path='/' component={Page} />
+        </Switch>
+      </div>
     </div>
   );
 }
