aa1a1e8e1319861ddb7cc1d7ca63b1cd282cb934
j-a-tran <jaclyn.a.tran@gmail.com>
2020-06-04 17:18:23 -0400

    added login/registration and basic routing

--- dinner_project/frontend/src/App.js
+++ dinner_project/frontend/src/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import Page from './components/Page';
+import Login from './components/Login';
+import Register from './components/Register';
+
+import { Switch, Route, NavLink, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Page />
+      <nav>
+        <ul>
+          <li>
+            <NavLink to='/register'>Register</NavLink>
+          </li>
+          <li>
+            <NavLink to='/login'>Log In</NavLink>
+          </li>
+          <li>
+            <NavLink to='/'>Home</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path='/register' component={Register} />
+        <Route path='/login' component={Login} />
+        <Route path='/' component={Page} />
+      </Switch>
     </div>
   );
 }
---
+++ dinner_project/frontend/src/components/Register.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function Register() {
+  const classes = useStyles();
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Typography component="h1" variant="h5">
+          Register
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="Username"
+            name="username"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Submit
+          </Button>
+          <Grid container>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Already have an account? Log in."}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
+  );
+}
