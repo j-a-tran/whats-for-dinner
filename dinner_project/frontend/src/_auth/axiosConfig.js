42cb681637706a1fa62727910709180d03985d8f
j-a-tran <jaclyn.a.tran@gmail.com>
2020-06-12 17:18:39 -0400

    axios interceptor updates

--- dinner_project/frontend/src/_auth/axiosConfig.js
+++ dinner_project/frontend/src/_auth/axiosConfig.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const API_URL = "http://127.0.0.1:8000/api/";
 
 let refreshInProgress = false;
+let queue = [];
 
 const axiosInstance = axios.create({
     baseURL: API_URL,
@@ -14,54 +15,83 @@ const axiosInstance = axios.create({
     }
 });
 
+const processQueue = (error, accessToken = null) => {
+    queue.forEach(promise => {
+        if (error) {
+            promise.reject(error);
+        } else {
+            promise.resolve(accessToken);
+        }
+    })
+
+    queue = [];
+}
+
 axiosInstance.interceptors.response.use(
     function (response) {
         //This triggers for anything where status code is within 2xx
         return response;
-    }, async function (error) {
+    }, function (error) {
 
         const originalRequest = error.config;
 
         if (error.response.status !== 401) {
+            console.log(error.response);
             return Promise.reject(error);
         }
 
         if (originalRequest.url === (API_URL + 'token/refresh/')) {
             localStorage.clear();
             window.location.href = '/login';
-            console.log(error);
+            console.log(error.response);
             return Promise.reject(error);
         }
 
         if (!refreshInProgress) {
-            try {
-                refreshInProgress = true;
-                const response = await axios.post(API_URL + 'token/refresh/', {
-                    refresh: localStorage.getItem('refresh_token')
-                });
-                refreshInProgress = false;
-                localStorage.setItem('access_token', response.data.access);
-                localStorage.setItem('refresh_token', response.data.refresh);
 
-                axiosInstance.defaults.headers['Authorization'] = 'JWT ' + response.data.access;
-                originalRequest.headers['Authorization'] = 'JWT ' + response.data.access;
+            originalRequest._retry = true;
+            refreshInProgress = true;
+
+            const refreshToken = localStorage.getItem('refresh_token');
+
+            return new Promise((resolve, reject) => {
+
+                axios.post(API_URL + 'token/refresh/', {
+                    refresh: refreshToken
+                })
+                .then((response) => {
+                    localStorage.setItem('access_token', response.data.access);
+                    localStorage.setItem('refresh_token', response.data.refresh);
+                    axiosInstance.defaults.headers['Authorization'] = 'JWT ' + response.data.access;
+                    originalRequest.headers['Authorization'] = 'JWT ' + response.data.access;
+
+                    processQueue(null, response.data.access);
+
+                    resolve(axiosInstance(originalRequest));
+                })
+                .catch((error) => {
+                    processQueue(error, null);
+                    reject(error);
+                })
+                .then(() => {
+                    refreshInProgress = false
+                })
+            })
+
+        } else {
+            return new Promise(function(resolve, reject) {
+                queue.push({resolve, reject})
+            })
+            .then(accessToken => {
+                originalRequest.headers['Authorization'] = 'JWT ' + accessToken;
                 return axiosInstance(originalRequest);
-            }
-            catch (error) {
-                refreshInProgress = false;
-                localStorage.clear();
-                window.location.href = '/login';
-                console.log(error);
+            })
+            .catch(error => {
                 return Promise.reject(error);
-            } 
+            })
         }
-        else {
-            //TODO: add 'sleep'
-            return axiosInstance(originalRequest);
-        }
-
     }
 );
 
 
-export { API_URL, axiosInstance};
+export { API_URL, axiosInstance };
