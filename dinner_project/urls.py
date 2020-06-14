feb0d74477318851267ac0319c17897a6310d6a8
j-a-tran <jaclyn.a.tran@gmail.com>
2020-06-14 03:22:19 -0400

    fixing API URL

--- dinner_project/urls.py
+++ dinner_project/urls.py
@@ -14,11 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path, re_path
-from django.views.generic import TemplateView
+from django.urls import include, path
 
 urlpatterns = [
     path('', include('recipes.urls')),
     path('admin/', admin.site.urls),
-    re_path('.*', TemplateView.as_view(template_name='index.html')),
 ]
--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from rest_framework_simplejwt import views as jwt_views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('api/recipes/', views.recipes_list),
@@ -10,5 +11,6 @@ urlpatterns = [
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token-refresh'),
     path('api/token/verify/', jwt_views.TokenVerifyView.as_view(), name='token-verify'),
     path('api/user/create/', views.create_user, name='user-create'),
-    path('api/token/blacklist/', views.logout_and_blacklist_token, name='blacklist')
+    path('api/token/blacklist/', views.logout_and_blacklist_token, name='blacklist'),
+    path('', TemplateView.as_view(template_name='index.html'))
 ]
